92b80357d0407c55c16dccfe30b06ea381738fb4
SamuelOluwayomi <samuelfaith500@gmail.com>
2026-06-18 12:41:03 +0100

    making minor improvements

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -41,10 +41,6 @@ const config: Config = {
       {
         docs: {
           sidebarPath: './sidebars.ts',
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         blog: {
           showReadingTime: true,
@@ -88,7 +84,7 @@ const config: Config = {
           label: 'Documentation',
         },
         {
-          href: 'https://github.com',
+          href: 'https://github.com/SamuelOluwayomi/smart-transaction-observatory',
           label: 'GitHub',
           position: 'right',
         },
